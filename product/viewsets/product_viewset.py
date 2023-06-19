7bb1bc255ff6dacdea478aaf1340d46c9def4fcf
Rafael Alves <rvf_alves@yahoo.com.br>
2023-06-20 00:48:50 +0100

    Authentication
    Development of authentication feature

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -7,7 +7,8 @@ from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
+	
 	serializer_class = ProductSerializer
 	
 	def get_queryset(self):
-		return Product.objects.all()
+		return Product.objects.all().order_by("id")
